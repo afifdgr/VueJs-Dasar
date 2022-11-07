c431e6765440c7b185a132cae9e99619f69b6526
afifdgr <afifdgr3@gmail.com>
2022-11-08 05:51:53 +0700

    Mengatur Layout

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -11,6 +11,21 @@
     <div class="item-logos">
       <img class="logo" alt="AV logo" src="../assets/aston_villa.png" />
     </div>
+    <div class="item-logos">
+      <img class="logo" alt="Forest logo" src="../assets/forest.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" alt="CP logo" src="../assets/crystal_palace.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" alt="AV logo" src="../assets/aston_villa.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" alt="Forest logo" src="../assets/forest.png" />
+    </div>
+    <div class="item-logos">
+      <img class="logo" alt="CP logo" src="../assets/crystal_palace.png" />
+    </div>
   </div>
 </template>
 
@@ -23,17 +38,18 @@ export default {
 <style scoped>
 .title-header {
   margin-bottom: 80px;
+  text-align: left;
 }
 .logos-wrapper {
-  width: 70%;
+  width: 62%;
   height: hidden;
   margin: auto;
 }
 
 .item-logos {
   float: left;
-  margin-right: 20px;
-  margin-bottom: 20px;
+  margin-right: 70px;
+  margin-bottom: 70px;
   width: 150px;
   height: 150px;
 }
