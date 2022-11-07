e63ab5fc523cf3104ebe992e1a0f9b066070ef41
afifdgr <afifdgr3@gmail.com>
2022-11-07 21:22:09 +0700

    Komponen #1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Logos />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logos from "./components/Logos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Logos,
+  },
+};
 </script>
 
 <style>
